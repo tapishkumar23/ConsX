ba940bd704bf061b7457761cf5667fbb96aae0b1
Tapish Kumar <tapish.ku123@gmail.com>
2026-04-29 14:38:39 +0530

    linking frontend to render deployed backend

--- src/components/AssignProject.tsx
+++ src/components/AssignProject.tsx
@@ -150,7 +150,7 @@ const AssignProject = ({ role, user }: any) => {
 
       if (!selectedUser?.email) return;
 
-      await fetch("http://localhost:5000/send-email", {
+      await fetch("https://consx.onrender.com/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
